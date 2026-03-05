198c20d1a6b7e1155edf59c016aadee65199b3ce
olucascdev <lucasdevproject@gmail.com>
2026-03-05 18:42:33 -0300

    feat: Custom validation rules to user request model and integrate a dedicated validation utility for binding errors.

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/olucascdev/crud-user-golang/src/configuration/validation"
 	"github.com/olucascdev/crud-user-golang/src/controller/model/request"
-	"github.com/olucascdev/crud-user-golang/src/controller/rest_err"
 )
 
 func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect filds,  error=%s\n", err.Error()))
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 
