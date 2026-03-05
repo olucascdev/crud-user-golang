e18501534f6f115cdf7b9f28be8483699297a839
olucascdev <lucasdevproject@gmail.com>
2026-03-05 17:29:27 -0300

    feat: Implement initial Gin server setup and CreateUser endpoint with request and response models.

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,21 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/olucascdev/crud-user-golang/src/controller/model/request"
+	"github.com/olucascdev/crud-user-golang/src/controller/rest_err"
+)
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect filds,  error=%s\n", err.Error()))
+		c.JSON(restErr.Code, restErr)
+		return
 
+	}
+	fmt.Println(userRequest)
 }
