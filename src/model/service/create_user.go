c295b9e0f34ec02706c5dd8a7242b9727869aa2c
olucascdev <lucasdevproject@gmail.com>
2026-03-17 22:15:42 -0300

    feat: add email uniqueness validation to prevent duplicate user registrations.

--- src/model/service/create_user.go
+++ src/model/service/create_user.go
@@ -13,6 +13,11 @@ func (ud *userDomainService) CreateUserServices(
 	logger.Info("Init createUser model",
 		zap.String("journey", "createUser"))
 
+	user, _ := ud.FindUserByEmailServices(userDomain.GetEmail())
+	if user != nil {
+		return nil, rest_err.NewBadRequestError("Email is already registered in another account")
+	}
+
 	userDomain.EncryptPassword()
 
 	userDomainRepository, err := ud.userRepository.CreateUser(userDomain)
